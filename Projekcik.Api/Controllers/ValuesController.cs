dff6b344ac0559222067e7380d1da7a39a277bfd
Dominik Ruman <dr124@users.noreply.github.com>
2019-03-08 23:33:13 +0100

    Update README.md

---
+++ Projekcik.Api/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projekcik.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static List<string> Values { get; set; } = new List<string>() { "witam", "pozdrawiam" };
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(Values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id >= Values.Count)
+                return BadRequest("id too big xd");
+
+            return Ok(Values[id]);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            Values.Add(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            if (id >= Values.Count)
+                throw new ArgumentOutOfRangeException(nameof(id));
+
+            Values[id] = value;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id >= Values.Count)
+                throw new ArgumentOutOfRangeException(nameof(id));
+
+            Values.RemoveAt(id);
+        }
+    }
+}
---
+++ Projekcik.Api/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Projekcik.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(c => { c.Filters.Add(new JsonExceptionFilter()); })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseMvc();
+        }
+    }
+
+    public class JsonExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var result = new ObjectResult(new
+            {
+                code = 500,
+                message = "A server error occurred.",
+                detailedMessage = context.Exception.Message
+            });
+
+            result.StatusCode = 500;
+            context.Result = result;
+        }
+    }
+}
