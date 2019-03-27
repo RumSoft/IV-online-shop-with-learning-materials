74e90bad7ee231e9daf3951cb05f2ab0141379f7
lukarson <48386569+lukarson@users.noreply.github.com>
2019-03-27 15:27:44 +0100

    Sample Update
    let's commit die

--- Projekcik.Client/src/pages/Login/index.js
+++ Projekcik.Client/src/pages/Login/index.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class LoginPage extends Component {
   render() {
-    return <div>login page</div>;
+    return <div>Tajne/poufne</div>;
   }
 }
