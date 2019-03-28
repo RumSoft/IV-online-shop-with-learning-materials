0a306e84513c686bc6b6dc43b29aad0e25189132
Dominik Ruman <dr124@o2.pl>
2019-03-28 23:36:41 +0100

    remove redux

--- Projekcik.Client/src/index.js
+++ Projekcik.Client/src/index.js
@@ -1,29 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
+import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import configureStore from './Store/configureStore';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const history = createBrowserHistory({ basename: baseUrl });
-
-// Get the application-wide store instance, prepopulating with state from the server where available.
-const initialState = window.initialReduxState;
-const store = configureStore(history, initialState);
-
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
+  <Router history={history}>
+    <App />
+  </Router>,
   rootElement
 );
 
