50bbb6829e75ce898b1ad325e0083213f48e816f
Dominik Ruman <dr124@o2.pl>
2019-03-28 23:48:33 +0100

    test image

--- Projekcik.Client/src/pages/Home/index.js
+++ Projekcik.Client/src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './index.scss';
+import img from './concept.JPG';
 
 export default class HomePage extends Component {
   render() {
     return (
       <div className="homepage">
         <h2>HomePage</h2>
+        <img src={img} className="concent" />
       </div>
     );
   }
