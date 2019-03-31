86a9a0d8dd65edb396e6cb27aed0fed94ef696e6
Szumekk <szumskijk@gmail.com>
2019-04-01 00:24:04 +0200

    feature/model/user

---
+++ Projekcik.Api/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekcik.Api.Models
+{
+    public class Entity<T>
+    {
+        public T Id { get; set; }
+    }
+}
---
+++ Projekcik.Api/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekcik.Api.Models
+{
+    public class User: Entity<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
