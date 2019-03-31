69f75cadb90624e2a3cdc55a31688047cb37ca58
Szumekk <szumskijk@gmail.com>
2019-04-01 00:31:51 +0200

    Update

--- Projekcik.Api/Models/Entity.cs
+++ Projekcik.Api/Models/Entity.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Projekcik.Api.Models
+﻿namespace Projekcik.Api.Models
 {
     public class Entity<T>
     {
--- Projekcik.Api/Models/User.cs
+++ Projekcik.Api/Models/User.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Projekcik.Api.Models
 {
-    public class User: Entity<int>
+    public class User : Entity<int>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
