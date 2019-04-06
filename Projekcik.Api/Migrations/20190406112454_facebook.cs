1cf2bbd10f02afe436669f07485b3d9cd8c0ce79
Dominik Ruman <dr124@o2.pl>
2019-04-06 13:29:44 +0200

    facebook migration

---
+++ Projekcik.Api/Migrations/20190406112454_facebook.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Projekcik.Api.Migrations
+{
+    public partial class facebook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "FacebookId",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FacebookId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Users");
+        }
+    }
+}
