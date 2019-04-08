9cff2e700dcc364bd9bf0fd81b270ba97e926273
Dominik Ruman <dr124@o2.pl>
2019-04-08 18:13:13 +0200

    login page changes && login panel

--- Projekcik.Client/src/Pages/Login/index.js
+++ Projekcik.Client/src/Pages/Login/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import AuthService from '../../Services/AuthService';
 import './index.scss';
+import LoginPanel from '../../Components/LoginPanel';
 
 /* TODO:
         - rozwinac ogolna funkcjonalnosc
@@ -12,62 +10,11 @@ import './index.scss';
 */
 
 export default class LoginPage extends Component {
-  state = {
-    emailAddress: "",
-    password: ""
-  };
-
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  };
-
-  handleSubmit = event => {
-    let getUrl = window.location;
-    let baseUrl = getUrl.protocol + "//" + getUrl.host;
-    event.preventDefault();
-    AuthService.login(this.state)
-        .then(data => window.localStorage.setItem('token', `${data.token}`))
-        .then(() => window.location.href = `${baseUrl}`);
-  };
-
   render() {
     return (
-      <form className="login-form" onSubmit={this.handleSubmit}>
-        
-        <div className="header">
-          <h1>Login Page v.Alpha</h1>
-        </div>
-
-        <div className="login-field">
-          <TextField
-            id="emailAddress"
-            label="Email Address"
-            variant="outlined"
-            value={this.state.emailAddress}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="login-field">
-          <TextField
-            id="password"
-            type="password"
-            label="Password"
-            variant="outlined"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-        </div>
-        <Button
-          type="submit"
-          className="submit"
-          variant="contained"
-          color="primary"
-          onClick={this.handleSubmit}>
-          Log In
-        </Button>
-      </form>
+      <div className="login-page">
+        <LoginPanel />
+      </div>
     );
   }
 }
