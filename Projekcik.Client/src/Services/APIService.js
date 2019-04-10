371a7b75dbcab536ac66afc51164e964f12a6bee
lukarson <48386569+lukarson@users.noreply.github.com>
2019-04-10 12:53:50 +0200

    add token expiration auth function

--- Projekcik.Client/src/Services/APIService.js
+++ Projekcik.Client/src/Services/APIService.js
@@ -1,45 +1,51 @@
 import axios from 'axios';
+import decode from 'jwt-decode';
 
-const API_URL = "https://projekcik-prz.azurewebsites.net";
+const API_URL = 'https://projekcik-prz.azurewebsites.net';
 
 export default class APIService {
-
-    static get (address) {
-
-        let token = window.localStorage.getItem('token')
-        const authHeader = {
-            headers: { 
-                'Authorization': `Bearer ${token}`
-            }
-        };
-
-        return axios.get(`${API_URL}/${address}`, authHeader)
-        .then(response => response.data);
-    }
-
-    static post (address, data) {
-        
-        let token = window.localStorage.getItem('token');
-        const authHeader = {
-            headers: { 
-                'Authorization': `Bearer ${token}`
-            }
-        };
-
-        return axios.post(`${API_URL}/${address}`, data, authHeader)
-        .then(response => {
-            window.localStorage.setItem('token', `${response.data.token}`);
-            return response.data;
-        });
-    }
-
-    static isAuthenticated () {
-        return window.localStorage.getItem('token') ? true : false;
-    }
-
-    static facebookLogin(accessToken) {
-        return APIService.post('/api/externalauth/facebook-login', {
-          accessToken: accessToken
-        });
+  static get(address) {
+    let token = window.localStorage.getItem('token');
+    const authHeader = {
+      headers: {
+        Authorization: `Bearer ${token}`
       }
-};
+    };
+
+    return axios
+      .get(`${API_URL}/${address}`, authHeader)
+      .then(response => response.data);
+  }
+
+  static post(address, data) {
+    let token = window.localStorage.getItem('token');
+    const authHeader = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+
+    return axios
+      .post(`${API_URL}/${address}`, data, authHeader)
+      .then(response => {
+        window.localStorage.setItem('token', `${response.data.token}`);
+        return response.data;
+      });
+  }
+
+  static isAuthenticated() {
+    let jwtToken = window.localStorage.getItem('token');
+    if (!jwtToken) {
+      return false;
+    } else {
+      let payload = decode(jwtToken);
+      return payload.exp > new Date().getTime() / 1000 ? true : false;
+    }
+  }
+
+  static facebookLogin(accessToken) {
+    return APIService.post('/api/externalauth/facebook-login', {
+      accessToken: accessToken
+    });
+  }
+}
