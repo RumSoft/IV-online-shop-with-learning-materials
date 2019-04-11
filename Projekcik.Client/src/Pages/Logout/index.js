494b721e0d17161b4b743a3cbe1fde785765306d
Dominik Ruman <dr124@o2.pl>
2019-04-11 16:38:33 +0200

    logout page

--- Projekcik.Client/src/Pages/Logout/index.js
+++ Projekcik.Client/src/Pages/Logout/index.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import { Card, CardContent } from '@material-ui/core';
+import { Card } from '@material-ui/core';
+import AuthService from '../../Services/AuthService';
+import './index.scss';
 
-export class Logout extends Component {
+export class LogoutPage extends Component {
   render() {
+    AuthService.logout();
+
+    let redirectUrl = `${window.location.protocol}//${window.location.host}`;
+
+    setTimeout(() => {
+      window.location.href = redirectUrl;
+    }, 2000);
+
     return (
-      <Card className="logged-out">
-        <CardContent>
-          <p>Wylogowano pomyślnie!</p>
-          <a href={`${window.location.protocol}//${window.location.host}`}>
-            Powrót na stronę główną
-          </a>
-        </CardContent>
-      </Card>
+      <div className="logged-out-page">
+        <Card className="logged-out-page-card">
+          <h3>Wylogowano pomyślnie!</h3>
+          <p>Zostaniesz przekierowany na stronę główną</p>
+          <hr />
+          <p>
+            w razie braku przekierowania kliknij <a href={redirectUrl}>tutaj</a>
+          </p>
+        </Card>
+      </div>
     );
   }
 }
 
-export default Logout;
+export default LogoutPage;
