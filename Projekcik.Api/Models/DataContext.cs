3d131d4fb435f7a6e7c358d66d5ee9ec188bcc43
Szumekk <szumskijk@gmail.com>
2019-04-11 11:30:10 +0200

    Note

--- Projekcik.Api/Models/DataContext.cs
+++ Projekcik.Api/Models/DataContext.cs
@@ -9,11 +9,14 @@ namespace Projekcik.Api.Models
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Note> Notes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             User.OnModelCreating(modelBuilder.Entity<User>());
             Subject.OnModelCreating(modelBuilder.Entity<Subject>());
+            Note.OnModelCreating(modelBuilder.Entity<Note>());
         }
     }
 }
---
+++ Projekcik.Api/Models/Note.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Projekcik.Api.Models
+{
+    public class Note : Entity<Guid>, ITimeStampedEntity
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public Subject SubjectName { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+
+        public static void OnModelCreating(EntityTypeBuilder<Note> entity)
+        {
+            entity.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(x => x.Author)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(x => x.SubjectName)
+                .IsRequired();
+
+            entity.Property(x => x.Price)
+                .IsRequired();
+
+            entity.Property(x => x.Description)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            entity.Property(x => x.CreatedAt)
+                .HasDefaultValueSql("getdate()")
+                .ValueGeneratedOnAdd();
+
+            entity.Property(x => x.ModifiedAt)
+                .HasDefaultValueSql("getdate()")
+                .ValueGeneratedOnAddOrUpdate();
+        }
+    }
+}
