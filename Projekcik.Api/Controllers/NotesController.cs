896641ad613c9cfac0ba35684d162a5c6c5205d5
Dominik Ruman <dr124@o2.pl>
2019-04-13 11:03:35 +0200

    basic file upload

--- Projekcik.Api/Controllers/NotesController.cs
+++ Projekcik.Api/Controllers/NotesController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Projekcik.Api.Helpers;
+using Projekcik.Api.Models;
 using Projekcik.Api.Models.DTO;
 using Projekcik.Api.Services;
 
@@ -16,13 +18,17 @@ namespace Projekcik.Api.Controllers
     [Route("api/[controller]")]
     public class NotesController : ControllerBase
     {
-        private readonly NoteService _noteService;
+        private readonly INoteService _noteService;
         private readonly IHttpContextAccessor _user;
-        public NotesController(NoteService noteService,
-            IHttpContextAccessor user)
+        private readonly IUserService _userService;
+
+        public NotesController(INoteService noteService,
+            IHttpContextAccessor user,
+            IUserService userService)
         {
             _noteService = noteService;
             _user = user;
+            _userService = userService;
         }
 
         [HttpGet("")]
@@ -37,6 +43,94 @@ namespace Projekcik.Api.Controllers
         {
             var notes = _noteService.GetNotesByAuthorId(userId);
             return notes.Select(Mapper.Map<NoteDto>);
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateNote(
+            [FromForm] IFormFile file,
+            [FromForm] string name,
+            [FromForm] string description,
+            [FromForm] decimal price,
+            [FromForm] Guid subject
+        )
+        {
+            if (string.IsNullOrWhiteSpace(name)
+                || string.IsNullOrWhiteSpace(description)
+                || price <= 0.0m)
+                return BadRequest("Input form data error");
+
+            if (file.Length <= 0)
+                return BadRequest("File error");
+
+            var allowedExtensions = new[] { "pdf" };
+            var extension = file.FileName.Split('.', StringSplitOptions.RemoveEmptyEntries).Last();
+            if (!allowedExtensions.Contains(extension.ToLower()))
+                return BadRequest("Unsupported file type");
+
+            var userId = _user.GetCurrentUserId();
+            var user = _userService.GetById(userId);
+            if (user == null)
+                return BadRequest("Invalid user");
+
+            var path = Path.Combine(
+                Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName,
+                "uploads",
+                userId.ToString());
+            Directory.CreateDirectory(path);
+
+            var noteId = Guid.NewGuid();
+            using (var fileStream = new FileStream(Path.Combine(path, noteId.ToString()), FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            var note = new Note
+            {
+                Id = noteId,
+                Author = user,
+                AuthorId = user.Id,
+                Name = name,
+                Price = price,
+                Description = description,
+                SubjectId = subject
+            };
+            _noteService.Create(note);
+
+            return Ok(new
+            {
+                note.Id,
+                path
+            });
+        }
+
+        [HttpGet("download-request/{noteId}")]
+        public IActionResult DownloadRequest(Guid noteId)
+        {
+            var userId = _user.GetCurrentUserId();
+            var user = _userService.GetById(userId);
+            if(user == null)
+                return BadRequest();
+
+            //if(!user has bought note)
+            //  return 400
+
+            var note = _noteService.GetNoteById(noteId);
+            if (note == null)
+                return NotFound();
+
+            var filepath = Path.Combine(
+                Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName,
+                "uploads",
+                note.Author.Id.ToString(),
+                note.Id.ToString());
+
+            var result =
+                new FileContentResult(System.IO.File.ReadAllBytes(filepath), "application/pdf")
+                {
+                    FileDownloadName = $"{note.Name}.pdf"
+                };
+
+            return result;
         }
     }
 }
