eb3e255b48b375a77ebb5dc12b5b944ab50cf758
Dominik Ruman <dr124@o2.pl>
2019-04-19 02:09:18 +0200

    cleanup & changes & hacks

--- Projekcik.Client/src/Components/NavBar/index.js
+++ Projekcik.Client/src/Components/NavBar/index.js
@@ -18,6 +18,7 @@ import './index.scss';
 import APIService from '../../Services/APIService';
 import SearchBar from '../SearchBar';
 import logo from './logoWhite.png';
+import { Tooltip } from '@material-ui/core';
 
 export default class NavBar extends Component {
   constructor(props) {
@@ -36,57 +37,71 @@ export default class NavBar extends Component {
 
   render() {
     let isLogged = APIService.isAuthenticated() ? true : false;
+    const menu = [
+      {
+        name: 'Strona główna',
+        to: '/',
+        icon: <AddIcon />,
+        hidden: false
+      },
+      {
+        name: 'Załóż konto',
+        to: '/register',
+        icon: <RegisterIcon />,
+        hidden: isLogged
+      },
+      {
+        name: 'Zaloguj się',
+        to: '/login',
+        icon: <LoginIcon />,
+        hidden: isLogged
+      },
+      {
+        name: 'Panel użytkownika',
+        to: '/protected',
+        icon: <AccountIcon />,
+        hidden: !isLogged
+      },
+      {
+        name: 'Wyloguj się',
+        to: '/logout',
+        icon: <LoginIcon />,
+        hidden: !isLogged
+      }
+    ];
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3">
-          <Container>
+        <Navbar
+          className="navbar navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3"
+          dark>
+          <Container className="navbar-container">
+            {/* first element only for equal flex spacing */}
+            <NavbarToggler onClick={this.toggle} className="hidden-toggler" />
             <NavbarBrand tag={Link} to="/">
-              <img src={logo} alt="ShopLogo" class="imgMobile imgDesktop" />
+              <img src={logo} alt="ShopLogo" class="navbar-logo" />
             </NavbarBrand>
-            <SearchBar />
-            <NavbarToggler onClick={this.toggle} className="menuWraper" />
+            <NavbarToggler onClick={this.toggle} />
             <Collapse
               className="d-sm-inline-flex flex-sm-row-reverse form-inline container"
-              isOpen={!this.state.isOpen}
+              isOpen={this.state.isOpen}
               navbar>
               <ul className="navbar-nav flex-grow ">
-              <div class="btn-group">
-                <NavItem hidden={isLogged}>
-                  <NavLink tag={Link} to="/">
-                    <button className="btn btn-default btn-light ">
-                      <AddIcon />
-                    </button>
-                  </NavLink>
-                </NavItem>
-                <NavItem hidden={isLogged}>
-                  <NavLink tag={Link} to="/register">
-                    <button className="btn btn-light ">
-                      <RegisterIcon />
-                    </button>
-                  </NavLink>
-                </NavItem>
-                <NavItem hidden={isLogged}>
-                  <NavLink tag={Link} to="/login">
-                    <button className="btn btn-light">
-                      <LoginIcon />
-                    </button>
-                  </NavLink>
-                </NavItem>
-                <NavItem hidden={!isLogged}>
-                  <NavLink tag={Link} className="text-dark" to="/protected">
-                    <button className="btn btn-light ">
-                      <AccountIcon />
-                    </button>
-                  </NavLink>
-                </NavItem>
-                <NavItem hidden={!isLogged}>
-                  <NavLink tag={Link} className="text-dark" to="/logout">
-                    <button className="btn btn-light ">
-                      <LogOutIcon />
-                    </button>
-                  </NavLink>
-                </NavItem>
-                </div>
+                <SearchBar className="nav-search" />
+                {menu.map((x, i) => (
+                  <NavItem key={i} hidden={x.hidden}>
+                    <NavLink tag={Link} className="text-dark" to={x.to}>
+                      <div className="d-sm-none link">
+                        {/* mobile */}
+                        {x.icon}
+                        <span>{x.name}</span>
+                      </div>
+                      <div className="d-none d-sm-block button btn btn-default">
+                        {/* large */}
+                        <Tooltip title={x.name}>{x.icon}</Tooltip>
+                      </div>
+                    </NavLink>
+                  </NavItem>
+                ))}
               </ul>
             </Collapse>
           </Container>
