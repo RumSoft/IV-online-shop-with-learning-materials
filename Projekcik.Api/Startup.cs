1f053da61dbbd13ceaffc23406d33cc0459e3f3f
Dominik Ruman <dr124@o2.pl>
2019-05-13 23:04:43 +0200

    payment service WIP

--- Projekcik.Api/Startup.cs
+++ Projekcik.Api/Startup.cs
@@ -80,6 +80,7 @@ namespace Projekcik.Api
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IHashService, PBKDF2HashSerivce>();
             services.AddScoped<INoteService, NoteService>();
+            services.AddSingleton<IPaymentService, PayUPaymentService>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
