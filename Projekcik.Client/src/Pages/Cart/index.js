3f830946297a78876d6162d0529543cf5b832036
lukarson <48386569+lukarson@users.noreply.github.com>
2019-05-21 03:59:52 +0200

    cart page

---
+++ Projekcik.Client/src/Pages/Cart/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ShoppingCart from '../../Components/ShoppingCart';
+
+export default class CartPage extends Component {
+  render() {
+    return (
+      <div>
+        <ShoppingCart />
+      </div>
+    );
+  }
+}
