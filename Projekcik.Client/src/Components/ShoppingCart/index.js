a3f5d47d9e9a8275011e83a67f80bd4d64e30cf9
lukarson <48386569+lukarson@users.noreply.github.com>
2019-05-21 18:34:07 +0200

    shopping cart update

--- Projekcik.Client/src/Components/ShoppingCart/index.js
+++ Projekcik.Client/src/Components/ShoppingCart/index.js
@@ -3,21 +3,27 @@ import { CircularProgress, Paper, Typography } from '@material-ui/core';
 import { Card, Button } from 'reactstrap';
 import { Redirect } from 'react-router-dom';
 import CartService from '../../Services/CartService';
-import NoteService from '../../Services/NoteService';
 
 import './index.scss';
 
 export default class ShoppingCart extends Component {
   constructor(props) {
     super(props);
-    NoteService.getAllNotes().then(r => this.setState({ ...r, loaded: true }));
+
+    CartService.getCartNotes().then(x => {
+      this.setState({ notes: x, loaded: true });
+    });
   }
 
   state = {
     loaded: false,
-    open: false,
     notes: [],
     totalPrice: 0
+  };
+
+  handleRemove = noteID => {
+    CartService.removeNoteFromCart(noteID);
+    window.location.reload();
   };
 
   redirectToNote = id => {
@@ -25,10 +31,8 @@ export default class ShoppingCart extends Component {
   };
 
   render() {
-    const { loaded, notes } = this.state;
-
+    const { loaded } = this.state;
     if (this.state.redirect) return <Redirect to={this.state.redirect} />;
-
     return (
       <Paper className="cart-list">
         <Typography variant="h5" className="my-cart">
@@ -36,69 +40,68 @@ export default class ShoppingCart extends Component {
         </Typography>
         <hr />
         {loaded ? (
-          notes.map((note, i) => (
-            <div>
-              <Card id={i} className="cart-note p-2 m-2">
-                <div className="note-image">
-                  <img
-                    className="p-2 m-2"
-                    style={{
-                      width: 128,
-                      height: 128
-                    }}
-                    src="http://placekitten.com/g/400/400"
-                    alt="notePreview"
-                  />
+          this.state.notes.map((note, i) => (
+            <Card key={i} className="cart-note p-2 m-2">
+              <div className="note-image">
+                <img
+                  className="p-2 m-2"
+                  style={{
+                    width: 128,
+                    height: 128
+                  }}
+                  src="http://placekitten.com/g/400/400"
+                  alt="notePreview"
+                />
 
-                  <div className="note-main">
-                    <Typography variant="caption" className="pb-2">
-                      {note.id}
+                <div className="note-main">
+                  <Typography variant="caption" className="pb-2">
+                    {note.id}
+                  </Typography>
+                  <Typography variant="h5" className="name pb-2">
+                    {note.name}
+                  </Typography>
+                  <Typography variant="subtitle2" className="pb-2">
+                    <i className="fa fa-user" />
+                    &nbsp; {note.author.name}
+                  </Typography>
+                </div>
+                <div className="note-origins">
+                  <div className="text">
+                    <Typography>
+                      <i className="fa fa-globe" />
+                      &nbsp; {note.voivodeship.name}
                     </Typography>
-                    <Typography variant="h5" className="pb-2">
-                      {note.name}
+                    <Typography>
+                      <i className="fa fa-university" />
+                      &nbsp; {note.university.name}
                     </Typography>
-                    <Typography variant="subtitle2" className="pb-2">
-                      <i className="fa fa-user" />
-                      &nbsp; {note.author.name}
+                    <Typography>
+                      <i className="fa fa-book" />
+                      &nbsp; {note.course.name}
                     </Typography>
                   </div>
-                  <div className="note-origins">
-                    <div className="text">
-                      <Typography>
-                        <i className="fa fa-globe" />
-                        &nbsp; {note.voivodeship.name}
-                      </Typography>
-                      <Typography>
-                        <i className="fa fa-university" />
-                        &nbsp; {note.university.name}
-                      </Typography>
-                      <Typography>
-                        <i className="fa fa-book" />
-                        &nbsp; {note.course.name}
-                      </Typography>
-                    </div>
-                    <div className="view-note">
-                      <button
-                        className="btn btn-md"
-                        onClick={() => this.redirectToNote(note.id)}>
-                        <i className="fa fa-book-open" />
-                        <span> Zobacz notatkę</span>
-                      </button>
-                    </div>
-                  </div>
-                  <div className="note-price">
-                    <Typography variant="h5" className="pb-3 mr-1">
-                      {note.price} zł
-                    </Typography>
-                    <Button
-                      type="button"
-                      className="btn btn-danger btn-circle btn-lg">
-                      <i className="fa fa-times" />
-                    </Button>
+                  <div className="view-note">
+                    <button
+                      className="btn btn-md"
+                      onClick={() => this.redirectToNote(note.id)}>
+                      <i className="fa fa-book-open" />
+                      <span> Zobacz notatkę</span>
+                    </button>
                   </div>
                 </div>
-              </Card>
-            </div>
+                <div className="note-price">
+                  <Typography variant="h5" className="pb-3 mr-1">
+                    {note.price} zł
+                  </Typography>
+                  <Button
+                    type="button"
+                    onClick={() => this.handleRemove(note.id)}
+                    className="btn btn-danger btn-circle btn-lg">
+                    <i className="fa fa-times" />
+                  </Button>
+                </div>
+              </div>
+            </Card>
           ))
         ) : (
           <div className="text-center">
