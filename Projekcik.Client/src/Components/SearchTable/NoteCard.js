da5f3cecae483e928370ef6c8987b38fd72413fb
lukarson <48386569+lukarson@users.noreply.github.com>
2019-05-21 18:34:36 +0200

    search notecard changes

--- Projekcik.Client/src/Components/SearchTable/NoteCard.js
+++ Projekcik.Client/src/Components/SearchTable/NoteCard.js
@@ -1,36 +1,106 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Grid, Card, Typography } from '@material-ui/core';
+import { Link, Redirect } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import {
+  Grid,
+  Card,
+  Typography,
+  Snackbar,
+  SnackbarContent
+} from '@material-ui/core';
+import CartService from '../../Services/CartService';
 import './index.scss';
 
-const NoteCard = ({ note }) => (
-  <Grid item sm={6}>
-    <Link to={`/note/${note.id}`} style={{ textDecoration: 'none' }}>
-      <Card className="note-card p-2 m-2">
-        <div className="note-image">
-          <img
-            className="p-2 m-2"
-            style={{
-              width: 128,
-              height: 128
-            }}
-            src="http://placekitten.com/g/400/400"
-            alt="notePreview"
-          />
-          <h5>{note.name}</h5>
-          <h6>{note.price} zł</h6>
-          <dl>
-            <Typography>Województwo: {note.voivodeship.name}</Typography>
-            <Typography />
-            <Typography>Uczelnia: {note.university.name}</Typography>{' '}
-            <Typography />
-            <Typography>Kierunek: {note.course.name}</Typography> <Typography />
-          </dl>
-        </div>
-        <div className="origin" />
-      </Card>
-    </Link>
-  </Grid>
-);
+export default class NoteCard extends React.Component {
+  state = {
+    redirect: null,
+    renderSnackbar: false,
+    open: false
+  };
 
-export default NoteCard;
+  handleClick = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    this.setState({ open: false });
+  };
+
+  redirectToNote = id => {
+    this.setState({ redirect: `/note/${id}` });
+  };
+
+  addToCart = noteID => {
+    CartService.addNoteToCart(noteID);
+    this.setState({ open: true });
+  };
+
+  render() {
+    const { note } = this.props;
+    if (this.state.redirect) return <Redirect to={this.state.redirect} />;
+
+    return (
+      <Grid item sm={6}>
+        <Card className="note-card p-2 m-2">
+          <div className="note-image">
+            <img
+              className="p-2 m-2"
+              style={{
+                width: 128,
+                height: 128
+              }}
+              src="http://placekitten.com/g/400/400"
+              alt="notePreview"
+            />
+            <div className="note-name">
+              <Link to={`/note/${note.id}`} style={{ textDecoration: 'none' }}>
+                <h5>{note.name}</h5>
+                <h6>{note.price} zł</h6>
+              </Link>
+            </div>
+            <dl>
+              <Typography>Województwo: {note.voivodeship.name}</Typography>
+              <Typography />
+              <Typography>Uczelnia: {note.university.name}</Typography>
+              <Typography />
+              <Typography>
+                Kierunek: {note.course.name}, sem. {note.semester}
+              </Typography>
+              <Typography />
+            </dl>
+          </div>
+          <div className="btn-group">
+            <Button
+              className="btn note btn-md rounded-right"
+              onClick={() => this.redirectToNote(note.id)}>
+              <i className="fa fa-book-open" />
+              <span> Zobacz notatkę</span>
+            </Button>
+            <Button
+              className="btn cart btn-md rounded-left"
+              onClick={() => this.addToCart(note.id)}>
+              <i className="fa fa-shopping-cart" />
+              <span> Dodaj do koszyka</span>
+            </Button>
+            <Snackbar
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left'
+              }}
+              open={this.state.open}
+              autoHideDuration={6000}
+              onClose={this.handleClose}>
+              <SnackbarContent
+                onClose={this.handleClose}
+                message="Dodano notatkę do koszyka!"
+              />
+            </Snackbar>
+          </div>
+        </Card>
+      </Grid>
+    );
+  }
+}
