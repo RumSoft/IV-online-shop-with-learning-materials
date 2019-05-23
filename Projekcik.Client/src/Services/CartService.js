ce2af4e663afca38974f7f2b7e095ee98a7b0f6f
Dominik Ruman <dr124@o2.pl>
2019-05-23 12:58:03 +0200

    Update CartService.js

--- Projekcik.Client/src/Services/CartService.js
+++ Projekcik.Client/src/Services/CartService.js
@@ -3,26 +3,26 @@ import NoteService from './NoteService';
 const cart = 'cartnotes';
 export default class CartService {
   static addNoteToCart(noteID) {
-    let noteArray = JSON.parse(window.localStorage.getItem(cart)) || [];
+    let noteArray = JSON.parse(window.localStorage.getItem(cart) || '[]') || [];
     noteArray.push(noteID);
     window.localStorage.setItem('cartnotes', JSON.stringify(noteArray));
   }
 
   static getCartNotes() {
-    let noteIDs = JSON.parse(window.localStorage.getItem(cart)) || [];
+    let noteIDs = JSON.parse(window.localStorage.getItem(cart) || '[]') || [];
     if (noteIDs && noteIDs.length)
       return NoteService.getNotesById(noteIDs).then(x => x);
     return this.emptyPromise([]);
   }
 
   static cartNoteCount() {
-    let noteIDs = JSON.parse(window.localStorage.getItem(cart)) || [];
+    let noteIDs = JSON.parse(window.localStorage.getItem(cart) || '[]') || [];
     if (noteIDs && noteIDs.length) return noteIDs.length;
     return 0;
   }
 
   static removeNoteFromCart(noteID) {
-    let noteArray = JSON.parse(window.localStorage.getItem(cart)) || [];
+    let noteArray = JSON.parse(window.localStorage.getItem(cart) || '[]') || [];
     for (var i = 0; i < noteArray.length; i++) {
       if (noteArray[i] === noteID) {
         noteArray.splice(i, 1);
@@ -34,7 +34,7 @@ export default class CartService {
   }
 
   static checkDuplicate(noteID) {
-    let noteArray = JSON.parse(window.localStorage.getItem(cart)) || [];
+    let noteArray = JSON.parse(window.localStorage.getItem(cart) || '[]') || [];
     return noteArray.includes(noteID) ? true : false;
   }
 
