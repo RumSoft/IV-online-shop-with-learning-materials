85fef09fcfff352bfd072917244706e6e6ecb34b
Dominik Ruman <dr124@o2.pl>
2019-05-23 12:00:32 +0200

    fix cart

--- Projekcik.Client/src/Services/CartService.js
+++ Projekcik.Client/src/Services/CartService.js
@@ -1,27 +1,28 @@
 import NoteService from './NoteService';
 
+const cart = 'cartnotes';
 export default class CartService {
   static addNoteToCart(noteID) {
-    let noteArray = JSON.parse(window.localStorage.getItem('cartnotes')) || [];
+    let noteArray = JSON.parse(window.localStorage.getItem(cart)) || [];
     noteArray.push(noteID);
     window.localStorage.setItem('cartnotes', JSON.stringify(noteArray));
   }
 
   static getCartNotes() {
-    let noteIDs = JSON.parse(window.localStorage.getItem('cartnotes'));
+    let noteIDs = JSON.parse(window.localStorage.getItem(cart)) || [];
     if (noteIDs && noteIDs.length)
       return NoteService.getNotesById(noteIDs).then(x => x);
     return this.emptyPromise([]);
   }
 
   static cartNoteCount() {
-    let noteIDs = JSON.parse(window.localStorage.getItem('cartnotes'));
+    let noteIDs = JSON.parse(window.localStorage.getItem(cart)) || [];
     if (noteIDs && noteIDs.length) return noteIDs.length;
     return 0;
   }
 
   static removeNoteFromCart(noteID) {
-    let noteArray = JSON.parse(window.localStorage.getItem('cartnotes'));
+    let noteArray = JSON.parse(window.localStorage.getItem(cart)) || [];
     for (var i = 0; i < noteArray.length; i++) {
       if (noteArray[i] === noteID) {
         noteArray.splice(i, 1);
@@ -29,16 +30,16 @@ export default class CartService {
         break;
       }
     }
-    window.localStorage.setItem('cartnotes', JSON.stringify(noteArray));
+    window.localStorage.setItem(cart, JSON.stringify(noteArray));
   }
 
   static checkDuplicate(noteID) {
-    let noteArray = JSON.parse(window.localStorage.getItem('cartnotes'));
+    let noteArray = JSON.parse(window.localStorage.getItem(cart)) || [];
     return noteArray.includes(noteID) ? true : false;
   }
 
   static clearNoteCart() {
-    window.localStorage.removeItem('cartnotes');
+    window.localStorage.removeItem(cart);
   }
 
   static emptyPromise(val = null) {
