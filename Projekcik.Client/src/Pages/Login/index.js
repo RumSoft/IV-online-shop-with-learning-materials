9942cbbfa64056f17003c398b36be75accd113d2
Dominik Ruman <dr124@users.noreply.github.com>
2019-06-06 09:12:12 +0200

    hotfix facebook login not being refreshed (#90)

--- Projekcik.Client/src/Pages/Login/index.js
+++ Projekcik.Client/src/Pages/Login/index.js
@@ -5,20 +5,22 @@ import AuthService from '../../Services/AuthService';
 import './index.scss';
 
 export default class LoginPage extends Component {
-  render() {
-    // facebook login
+  state = {};
+  componentWillMount() {
     let params = queryString.parse(this.props.location.hash);
     if (params.access_token) {
-      AuthService.facebookLogin(params.access_token)
-        .then(data => AuthService.handleLogin(data))
-        .catch(x => console.log(x));
+      AuthService.facebookLogin(params.access_token).then(data => {
+        AuthService.handleLogin(data);
+        this.setState({ logged: true });
+      });
     }
-
+  }
+  render() {
     var redirectData =
       this.props.location.state && this.props.location.state.from;
     return (
       <div className="login-page">
-        <LoginPanel redirectData={redirectData} />
+        <LoginPanel redirectData={redirectData} logged={this.state.logged} />
       </div>
     );
   }
